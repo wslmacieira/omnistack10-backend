02286d9cd6f28c9a9284edf0290f8b29269ead6c
wslmacieira <wslmacieira@DESKTOP-GJDMPV4.localdomain>
2020-01-14 12:08:23 -0300

    criando conexão ao banco de dados

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9-b4ne1.mongodb.net/week10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+})
 
 app.use(express.json())
 
@@ -11,6 +17,8 @@ app.use(express.json())
 // Query Params: request.query (Filtros, ordenação, paginação, ...)
 // Route Params: request.params (Identifica um recurso na alteração ou remoção)
 // Body: request.body (Dados para criação ou alteração de um registro)
+
+// MongoDB (Não-relacional)
 
 app.post('/users', (request, response) => {
   console.log(request.body)
